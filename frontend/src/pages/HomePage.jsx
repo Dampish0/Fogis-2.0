fbdb880adf7934273d2118909b4c4c03ed806c8c
Dampish0 <elias.dovkrans@gmail.com>
2025-09-22 15:55:01 +0200

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import NavBar from '../components/navbar';
+import NavBar from '../components/NavBar.jsx';
 import Button from '@mui/material/Button';
 import toast from 'react-hot-toast';
 import '@fontsource/roboto/500.css';
