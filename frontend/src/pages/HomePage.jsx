48c2f56c2b0abf62b017c37720a068b709fcc35d
Dampish0 <elias.dovkrans@gmail.com>
2025-09-22 15:28:26 +0200

    fixed font and backend code

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import NavBar from '../components/navbar';
 import Button from '@mui/material/Button';
 import toast from 'react-hot-toast';
+import '@fontsource/roboto/500.css';
 
 const HomePage = () => {
   return (
-    <div className='min-h-screen'>
+    <div className='min-h-screen' style={{ fontFamily: 'Roboto, sans-serif' }}>
       <NavBar/>
-      <Button onClick={() => toast.success("Congrats unc")}>do not click me</Button>
-
-
+      <Button onClick={() => toast.success("Congrats unc", {
+        style: { fontFamily: 'Roboto, sans-serif' }
+      })}>
+        do not click me
+      </Button>
     </div>
   )
 }
