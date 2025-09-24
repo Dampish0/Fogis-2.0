6c6a4c587586c40ad33687276184246d42ddafe1
Samuel Yousef <samyousef@live.com>
2025-09-24 16:05:53 +0200

    m

--- frontend/src/components/MatchDetail.jsx
+++ frontend/src/components/MatchDetail.jsx
@@ -1,12 +1,16 @@
 import eif from '../assets/EIF.png'
 import { Avatar } from '@mui/material';
 
+
 function MatchDetail() {
     return(
         
         <div className="matchdetalj">
-
-            <img className="logo" src={eif} alt="Team logo" ></img>
+        <div><Avatar 
+        alt="Team logo" 
+        src={eif}
+        sx={{ width:50, height: 50}} /></div>
+            
             <h2>Ekhagens IF</h2>
 
         </div>
--- frontend/src/pages/MatcherPage.jsx
+++ frontend/src/pages/MatcherPage.jsx
@@ -1,18 +1,36 @@
-import Container from '@mui/material/Container'
-import React from 'react'
 import MatchDetail from '../components/MatchDetail'
-import NavBar from '../components/Navbar'
-import ListMUI from '../components/ListMUI'
-import { Box, Divider } from '@mui/material'
+import { Box, Container } from '@mui/material';
+
 
 function MatcherPage() {
     return(
-        <div style={{
-            background: "#ebebebff", padding: "10%",
-        }}>
-            
-        <MatchDetail></MatchDetail>
-
+        <div style={{background: "#102542", minHeight: "100vh"}}>
+            <MatchDetail />
+            <Container fixed>
+                <Box sx={{ display: "flex", gap: 3 }}>
+                    <Box>
+                        <div>Tidigare Matcher</div>
+                        <Box className="TidigareMatcher" 
+                            sx={{   
+                                bgcolor: "#f5f5f5", 
+                                height: "500px",
+                                width: "250px", 
+                            }} 
+                        />
+                    </Box>
+                    <Box>
+                        <div>Kommande Matcher</div>
+                        <Box className="TidigareMatcher" 
+                            sx={{   
+                                bgcolor: "#f5f5f5", 
+                                height: "500px",
+                                width: "250px",
+                                
+                            }} 
+                        />
+                    </Box>
+                </Box>
+            </Container>
         </div>
     );
 }
