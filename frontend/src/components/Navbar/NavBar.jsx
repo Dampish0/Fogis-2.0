34d89f0487ee75a360bb0a52838d746990f6fb64
Filip <filip_yousif@hotmail.com>
2025-09-27 19:28:14 +0200

    Prototype of Navbar

--- frontend/src/components/Navbar/NavBar.jsx
+++ frontend/src/components/Navbar/NavBar.jsx
@@ -12,43 +12,77 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
-import clublogo from '../../assets/hff.jpg'
-import HomeIcon from '@mui/icons-material/Home';
-import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
-import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
-import './navbar.css'
+import SettingIcon from '@mui/icons-material/Settings';
+import { Divider } from '@mui/material';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import eif from '../../assets/hff.png';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+const NavBar = (props) => {
 
 
+  return (<>
+    <div style={{
+      ...props.style,
+      position: "absolute",
+      overflow: "hidden",
+      background: "rgba(30, 30, 30, 0.7)",
+      backdropFilter: "blur(12px)", 
+      WebkitBackdropFilter: "blur(12px)",
+      height: "60px",
+      maxWidth: "100vw",
+      borderRadius: "20px",
+      boxShadow: "0 4px 16px rgba(0, 0, 0, 0.7)",
+      //margin: "16px",
+      left: "16px",
+      right: "16px",
+      top: "16px",
+      marginBottom: "16px",
+      border: "1px solid rgba(255,255,255,0.08)",
+    }}>
+      <div style={{ display: "flex", alignItems: "center", height: "100%", flexDirection: "row" }}>
+        <Typography style={{marginLeft: "10px", letterSpacing: "4px"}} variant="h5" component="div" sx={{ color: "white", padding: "16px" }}>
+          FAIS
+        </Typography>
+        <Button sx={{fontSize: "16px",marginLeft: "10px", color: "white", transition: "transform 200ms ease", "&:hover": {transform: 'scale(1.1)'}}}>
+          Hem
+        </Button>
+        <Divider orientation="vertical" flexItem sx={{alignSelf: "center", height: "50%", bgcolor: "rgb(255, 255, 255, 0.5)", mx: 2 }} />
+        <Button sx={{fontSize: "16px", marginLeft: "10px", color: "white", transition: "transform 200ms ease", "&:hover": {transform: 'scale(1.1)'}}}>
+          Matcher
+        </Button>
+        <Divider orientation="vertical" flexItem sx={{alignSelf: "center", height: "50%", bgcolor: "rgb(255, 255, 255, 0.5)", mx: 2 }} />
+        <Button sx={{fontSize: "16px",marginLeft: "10px", color: "white", transition: "transform 200ms ease", "&:hover": {transform: 'scale(1.1)'}}}>
+          Tävlingar
+        </Button>
+
+        {/* mitten */}
 
 
+        {/* HÖGRA SIDAN */}
+        <IconButton size="large"
+         sx={{alignSelf: "right", color: "white", marginLeft: "auto" }}>
+          <NotificationsIcon />
+        </IconButton>
+        <Divider orientation="vertical" flexItem sx={{alignSelf: "center", height: "50%", bgcolor: "rgb(255, 255, 255, 0.5)", mx: 2 }} />
+        <Button style={{color: "white", marginRight: "10px"}}>
+          <KeyboardArrowDownIcon sx={{ color: "white" }}/>
 
-function NavBar() {
-  return (
-    <nav className='NavBar'>
-      
-       <img src={clublogo} alt="clublogo" />
-       <h1 className="clubname">Husqvarna FF</h1>
-      
-      <div className='icons'>
-        <div className='title'>
-      <HomeIcon className= "icon" style={{fontSize : "150%", cursor : "pointer" }}/> 
-        <p>Hem</p>
-        </div>
-      
-         <div className='title'>
-      <SportsSoccerIcon className="icon" style={{fontSize : "150%", cursor : "pointer"}}/> 
-        <p>Matcher</p>
-        </div>
-       
-        <div className='title'>
-      <EmojiEventsIcon className="icon" style={{fontSize : "150%", cursor : "pointer"}}/> 
-       <p>Tävlingar</p>
-        </div>
-       </div>
-      <Avatar sx = {{width:40, height: 40, cursor: "pointer"}}/>
-
-    </nav>
+          <Typography 
+          variant="h6" component="div" sx={{ color: "white", padding: "16px 2px 16px 2px"  }}
+          >
+            Huskvarna IF
+          </Typography>
+          <Avatar src={eif} sx={{ width: 32, height: 32, marginLeft: "8px" }}>
+          
+          </Avatar>
+        </Button>
+      </div>
+    </div>
+    <div style={{height: "92px"}}></div>
+    </>
   );
+
 }
 
 export default NavBar
