4c5b1e5cbcb3d7302bd700f505bb9384e7fac306
Filip <filip_yousif@hotmail.com>
2025-09-27 19:51:05 +0200

    update homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
-import NavBar from '../components/Navbar/NavBar';
+import NavBar from '../components/NavBar.jsx';
 import Button from '@mui/material/Button';
 import toast from 'react-hot-toast';
+import '@fontsource/roboto/500.css';
 
 const HomePage = () => {
   return (
-    <div className='min-h-screen'>
+    <div style={{backgroundColor: "#FFFFFF",
+     overflow: "hidden",
+      fontFamily: 'Roboto, sans-serif' }}>
       <NavBar/>
-      <Button onClick={() => toast.success("Congrats unc")}>do not click me</Button>
-
-
+      <Button onClick={() => toast.success("Congrats unc", {
+        style: { fontFamily: 'Roboto, sans-serif' }
+      })}>
+        do not click me
+      </Button>
     </div>
   )
 }
