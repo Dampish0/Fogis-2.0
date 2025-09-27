6fa55c1c8c1dec0a599196869396d57f1bb5eafd
Dampish0 <elias.dovkrans@gmail.com>
2025-09-27 19:51:30 +0200

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -6,7 +6,9 @@ import '@fontsource/roboto/500.css';
 
 const HomePage = () => {
   return (
-    <div className='min-h-screen' style={{ fontFamily: 'Roboto, sans-serif' }}>
+    <div style={{backgroundColor: "#FFFFFF",
+     overflow: "hidden",
+      fontFamily: 'Roboto, sans-serif' }}>
       <NavBar/>
       <Button onClick={() => toast.success("Congrats unc", {
         style: { fontFamily: 'Roboto, sans-serif' }
