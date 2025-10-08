4d7f5af30937d91aaf92697e09fd1246ea9f6c64
John <john-molin@hotmail.com>
2025-10-08 16:11:00 +0200

    Update CompetitionDetails.jsx

--- frontend/src/pages/CompetitionPage/CompetitionDetails.jsx
+++ frontend/src/pages/CompetitionPage/CompetitionDetails.jsx
@@ -4,8 +4,12 @@ import { Button, Typography, Box } from '@mui/material';
 export default function CompetitionDetails({ type, id, name, onBack }) {
   return (
     <Box sx={{ py: 2 }}>
-      <Typography variant="h4" sx={{ fontWeight: 700, mb: 1 }}>
+      <Typography variant="h4" sx={{ fontWeight: 700, mb: 3 }}>
         {name ?? `#${id}`}
+      </Typography>
+
+      <Typography sx={{ mt: 2, fontSize: '1.5rem', fontWeight: 700 }}>
+        Senaste matcherna
       </Typography>
 
     </Box>
